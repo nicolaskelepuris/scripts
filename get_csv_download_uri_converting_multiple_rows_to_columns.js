7315954508c3650e3f8608b7b39e19cd1230f11e
nicolas <nicolas.kelepuris@gmail.com>
2021-10-20 15:35:12 -0300

    fix indentation

--- get_csv_download_uri_converting_multiple_rows_to_columns.js
+++ get_csv_download_uri_converting_multiple_rows_to_columns.js
@@ -9,20 +9,20 @@ const input = {
 }
 
 const convertRawContentToRows = function (input) {
-	const content = input.rawContent.split('\n')
+  const content = input.rawContent.split('\n')
   const rows = []
   content.forEach((item, index) => {
-	if ((index + 1) % input.numberOfRows === 0) {
+    if ((index + 1) % input.numberOfRows === 0) {
       rows.push(content.slice(index - (input.numberOfRows - 1), index + 1))
     }
   })
-  
+
   return rows
 }
 
 const rows = convertRawContentToRows(input)
 
-const createDownloadUriFromRows = function(rows) {
+const createDownloadUriFromRows = function (rows) {
   const csv = 'data:text/csv;charset=utf-8,\uFEFF' + rows.join('\r\n')
 
   return encodeURI(csv)
